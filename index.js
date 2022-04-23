9ef97eaa188c151a7f088fbcc1b56ed1954deaca
Mak Alamin <makalamin012@gmail.com>
2022-04-24 04:39:20 +0600

    get all products

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 
@@ -10,6 +12,35 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello From Natural Shop server.");
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdfnv.mongodb.net/natural_shop?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("DB Connected");
+
+    const productCollection = client.db("natural_shop").collection("products");
+
+    app.get("/products", async (req, res) => {
+      let cursor = productCollection.find({});
+      let products = await cursor.toArray();
+      res.send(products);
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Listening to http://localhost:" + port);
